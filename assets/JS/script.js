41284b0cf280c53bcbbf0bdfbd565645ca445e97
Kelvin Chan <kchan.dbs@gmail.com>
2024-11-28 13:42:33 +0000

    add hmtl content to be added when createGame function is called

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,7 +13,34 @@ const createGame = () => {
     document.getElementById('play-button').remove();
     //create html elements
     const gameArea = document.getElementById('play-area');
-    gameArea.innerHTML = ``;
+    gameArea.innerHTML = `<section id="container">
+            <div class="row">
+                <div class="col-12 col-md-6 d-flex justify-content-center text-center">
+                    <div class="row">
+                        <div class="col-12">
+                            <input type="number" id="input-number">
+                        </div>
+                        <div class="col-12">
+                            <button type="button" class="btn btn-primary" id="submit-button">Submit</button>
+                        </div>
+                        <div class="col-12">
+                            <button type="button" class="btn btn-primary" id="reset-button">Reset</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-12 col-md-6 d-flex justify-content-center text-center">
+                    <div class="row">
+                        <div class="col-12">
+                            <p>Current Total: <span>0</span></p>
+                        </div>
+                        <div class="col-12">
+                            <p>turns: <span>5</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>`;
 }
 
 //add event listener to play button
