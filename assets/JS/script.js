1249fd8280e321c2ec5f9a0f48e9cd45edfd4d3e
Kelvin Chan <kchan.dbs@gmail.com>
2024-11-28 16:41:00 +0000

    create function for phase 1 of game which is called when submit is clicked

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,14 @@
-//1.generate game html
- //1.1. remove intro area
- //1.2. remove play button
- //1.3 create html elements
+//Play Button
+    //1.generate game html
+    //1.1. remove intro area
+    //1.2. remove play button
+    //1.3 create html elements
 //2. add event listener to play button to call the function
 
 
-//generate game html
+/**
+ * Creates the html elements for the game
+ */
 const createGame = () => {
     //remove intro area
     document.getElementById('intro-area').remove();
@@ -39,7 +42,48 @@ const createGame = () => {
                         </div>
                     </div>
                 </div>`;
+
+    //add event listener to submit button
+    document.getElementById('submit-button').addEventListener('click', gamePhaseOne);
 }
 
 //add event listener to play button
 document.getElementById('play-button').addEventListener('click', createGame);
+
+//Game Phase 1
+//1. submit button
+    //1.1. generate random number
+    //1.2. get input value, check if input is valid
+    //1.3. if valid calculate difference of input number and generated number
+    //1.4. builds new content of input area
+//2. add event listener to submit button
+
+
+/** 
+ * generates a random number between 1 and 10
+ * reads the input value and checks if it is valid
+ * returns myRoll which is the difference between the random number and the input number
+ */
+const gamePhaseOne = () => {
+    let randomNumber = Math.floor(Math.random() * 10) + 1; //generate random number
+    let userNumber = parseInt(document.getElementById('input-number').value); //get input value
+    if (userNumber < 1 || userNumber > 10 || isNaN(userNumber)) { //check if input is valid
+        alert('Please enter a number between 1 and 10');
+        return;
+    } else {
+        let myRoll = Math.abs(randomNumber - userNumber); //calculate difference
+        const inputArea = document.getElementById('input-area'); //replace input area content
+        inputArea.innerHTML = `<div class="row">
+                            <div class="col-12 my-3">
+                                <div id="my-roll">${myRoll}</div>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <button type="button" class="btn btn-primary" id="subtract-button">Subtract</button>
+                                <button type="button" class="btn btn-primary" id="addition-button">Add</button>
+                            </div>
+                            <div class="col-12 mb-3">
+                                <button type="button" class="btn btn-primary" id="reset-button">Reset</button>
+                            </div>
+                        </div>`;           
+    }
+}
